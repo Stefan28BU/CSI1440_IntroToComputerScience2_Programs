4230d080bd3b7328b1a8f2f2570f0dc4eaa40dd8
Yufan Xu <yufanxu@YufandeMacBook-Pro.local>
2020-01-15 23:27:36 -0600

    final version

---
+++ project14401/cmake-build-debug/CMakeFiles/project_14401.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/project_14401.dir/main.cpp.o"
+  "project_14401.pdb"
+  "project_14401"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/project_14401.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
